e2fe5a9b409ad9a12eafa6a3d549a4c4b76ee869
Shivam Jadhav <34097420+Flash-Shivam@users.noreply.github.com>
2020-02-27 00:02:17 +0530

    Central Authority

--- A3/central.py
+++ A3/central.py
@@ -1,5 +1,5 @@
 import sympy
-from helper import strong_prime,D_Rsa
+from helper import strong_prime, d_Rsa, text_to_int, int_to_text
 
 
 # Generate Central Authority's Private Key
@@ -13,5 +13,10 @@ n = strong_prime(b,a)
 
 num = 2
 
+sign_pk = []
+
 for i in range(1,num+1):
-    
+    public_key = "User" + str(sympy.randprime(num,1000))
+    sign_pk.append((int_to_text(d_Rsa(public_key,int_to_text(PrivateKeyCA),n)),public_key))
+
+print(sign_pk)
